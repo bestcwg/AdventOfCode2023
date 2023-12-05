e1f34e7afdc9db8bc26c7947e5cbdbefb389fb20
christiangrann <Christian.Grann@BoostPLM.com>
2023-12-05 01:12:20 +0100

    Setup for day4 part2

---
+++ Day4/Part2.cs
@@ -0,0 +1,11 @@
+using Commen.Interface;
+
+namespace Day4;
+
+public class Part2 : Solve
+{
+    public int Solve()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Day4;
 
 var part1 = new Part1();
+var part2 = new Part2();
 
 Console.WriteLine(part1.Solve());
+Console.WriteLine(part2.Solve());
 
